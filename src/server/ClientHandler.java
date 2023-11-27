6e1f62b7a92a877b44f722ae9ee4ab8115a044d7
刘尚 <109736060+van-darkness@users.noreply.github.com>
2023-11-27 12:00:28 +0000

    创建服务端的三个类：大厅、房间、但对客户端处理

---
+++ src/server/ClientHandler.java
@@ -0,0 +1,5 @@
+package src.server;
+
+public class ClientHandler {
+    
+}
---
+++ src/server/Room.java
@@ -0,0 +1,11 @@
+package src.server;
+
+public class Room implements Runnable{
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+    
+}
