116875a5760b7f4c92b6cf5090339f3ba51833d3
刘尚 <109736060+van-darkness@users.noreply.github.com>
2023-11-27 12:39:04 +0000

    添加大厅类，初步编写四个类

--- src/server/Room.java
+++ src/server/Room.java
@@ -1,11 +1,47 @@
 package src.server;
 
-public class Room implements Runnable{
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Room implements Runnable {
+    private String name;
+    private List<ClientHandler> clients;
+
+    public Room(String name) {
+        this.name = name;
+        this.clients = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addClient(ClientHandler client) {
+        clients.add(client);
+    }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        // 在这里处理房间的消息传递逻辑
+        // 可以添加更多的逻辑，例如处理客户端消息、向所有客户端广播消息等
+        while (true) {
+            // 这里可以添加处理消息的逻辑
+        }
     }
-    
+
+    // 向房间内的所有客户端广播消息
+    public void broadcastMessage(String message) {
+        for (ClientHandler client : clients) {
+            client.sendMessage(message);
+        }
+    }
+
+    // 移除房间内的客户端
+    public void removeClient(ClientHandler client) {
+        clients.remove(client);
+    }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,59 @@
 package src.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
-    
+    private ServerSocket serverSocket;
+    private List<Room> rooms;
+
+    public Server(int port) {
+        rooms = new ArrayList<>();
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server is running on port " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        while (true) {
+            try {
+                // 等待客户端连接
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+                // 获取或创建房间
+                Room room = getOrCreateRoom("General");
+                ClientHandler clientHandler=new ClientHandler(clientSocket, room);
+                // 将客户端加入房间
+                room.addClient(clientHandler);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Room getOrCreateRoom(String roomName) {
+        for (Room room : rooms) {
+            if (room.getName().equals(roomName)) {
+                return room;
+            }
+        }
+        // 如果房间不存在，创建新房间并启动线程
+        Room newRoom = new Room(roomName);
+        rooms.add(newRoom);
+        new Thread(newRoom).start();
+        return newRoom;
+    }
+
+    public static void main(String[] args) {
+        int port = 8888; // 服务器端口
+        Server server = new Server(port);
+        server.start();
+    }
 }
